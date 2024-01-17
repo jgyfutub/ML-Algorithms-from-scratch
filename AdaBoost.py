cba529cc814f241ee1934e7d6d6f1b8e51da09d8
Vedant Pandey <vedantpandey459@gmail.com>
2024-01-17 22:18:42 +0530

    adaboost done

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import math
 from collections import Counter
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
@@ -8,18 +9,11 @@ import warnings
 warnings.filterwarnings('ignore')
 
 data = {
-    "Animal": ["Dog", "Dog", "Bird", "Fish", "Snake", "Tiger", "Tiger", "Elephant", "Monkey", "Bear"],
-    "Fruit": ["Apple", "Apple", "Apple", "Grapefruit", "Strawberry", "Strawberry", "Mango", "Mango", "Kiwi", "Melon"],
-    "Color": ["Red", "Red", "Red", "Yellow", "Blue", "Blue", "Pink", "Brown", "Brown", "Brown"],
-    "City": ["London", "London", "Tokyo", "New York", "Berlin", "Rome", "Rome", "Beijing", "Moscow", "Sydney"],
-    "Country": ["USA", "USA", "Mexico", "Brazil", "Argentina", "Chile", "Peru", "Colombia", "Venezuela", "Ecuador"],
-    "Job": ["Doctor", "Doctor", "Teacher", "Teacher", "Engineer", "Engineer", "Musician", "Musician", "Athlete", "Athlete"],
-    "Hobby": ["Reading", "Reading", "Writing", "Writing", "Movies", "Movies", "Games", "Games", "Camping", "Camping"],
-    "Movie": ["Action", "Action", "Comedy", "Comedy", "Romance", "Romance", "Thriller", "Thriller", "Horror", "Horror"],
-    "Book": ["The Lord of the Rings", "The Lord of the Rings", "Harry Potter", "Harry Potter", "To Kill a Mockingbird", "To Kill a Mockingbird", "1984", "1984", "Animal Farm", "Animal Farm"],
-    "Song": ["Bohemian Rhapsody", "Bohemian Rhapsody", "Imagine", "Imagine", "Hallelujah", "Hallelujah", "Yesterday", "Yesterday", "Happy", "Happy"],
-    'target': ['Yes', 'No', 'Yes', 'No', 'Yes','No','No','Yes','No','No']
-}
+    'cgpa':["y","n","y","n","y","y"],
+    "interactiveness":["g","g","a","a","g","g"],
+    "practical":["g","m","m","g","m","m"],
+    "communication":["y","n","n","n","y","y"],
+    'target': ["y","y","n","n","y","y"]}
 
 def shuffle_within_columns(data):
     for col in data:
@@ -36,15 +30,44 @@ def Counterdata(array):
 def AdaBoostAlgorithm(data,targetname):
     for i in data.columns.tolist():
         data[i]=[float(dict(Counterdata(list(data[i])))[j]) for j in data[i]]
-    weights=[1/len(data[targetname])]*len(data[targetname])
     bootstrap_samples = data.sample(frac=1, replace=True)
     target=bootstrap_samples[targetname]
+    weights=[1/len(target)]*len(target)
     bootstrap_samples.drop(["target"],axis=1, inplace=True)
-    print(data['target'])
-    for i in bootstrap_samples.columns.tolist():
+    print(bootstrap_samples.columns.tolist())
+    alphainstances={}
+    for j in bootstrap_samples.columns.tolist():
+        error=0
+        alpha=0
+        normalizing_factor=0
+        correct_weights=[]
+        incorrect_weights=[]
+        dict1={}
+        print(len(target))
         clf = tree.DecisionTreeClassifier(max_depth=1,).fit(bootstrap_samples,target,sample_weight=weights)
-        prediction=clf.predict([bootstrap_samples[i]])
-        print(prediction)
+        prediction=clf.predict(bootstrap_samples)
+        for i in range(len(target)):
+            # print(error)
+            if prediction[i]==list(target)[i]:
+                error=error+weights[i]
+                correct_weights.append(weights[i])
+                dict1[i]=1
+            else:
+                incorrect_weights.append(weights[i])
+                dict1[i]=0
+        if error>1:
+            alphainstances[j]="useless"
+            continue
+        alpha=math.log((1-error)/error)*0.5
+        normalizing_factor=sum(correct_weights)*math.exp(-alpha)+sum(incorrect_weights)*math.exp(alpha)
+        print(normalizing_factor,alpha,error,correct_weights,incorrect_weights)
+        for i in dict1:
+            if dict1[i]==1:
+                weights[i]*=math.exp(-alpha)/normalizing_factor
+            else:
+                weights[i]*=math.exp(alpha)/normalizing_factor
+        alphainstances[j]=alpha
+    return alphainstances
 print(AdaBoostAlgorithm(df,'target'))
 
 
